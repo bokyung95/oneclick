6c791fa591f83fc8b6e50db3cdc0674352cc6d42
bokyung <qhrud7915@naver.com>
2022-10-25 23:52:00 +0900

    Initial commit

---
+++ src/main/java/com/gang/oneclickshoppingmall/OneclickShoppingmallApplication.java
@@ -0,0 +1,13 @@
+package com.gang.oneclickshoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneclickShoppingmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneclickShoppingmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gang/oneclickshoppingmall/OneclickShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gang.oneclickshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneclickShoppingmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
