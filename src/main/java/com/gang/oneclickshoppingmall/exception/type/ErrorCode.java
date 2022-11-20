d01633e6a4e356ad8af70fcefa491fc2ac83d44a
bokyung <qhrud7915@naver.com>
2022-11-21 00:42:34 +0900

    feat: 회원 가입 validation 추가
    회원 가입 validation 추가

--- src/main/java/com/gang/oneclickshoppingmall/exception/type/ErrorCode.java
+++ src/main/java/com/gang/oneclickshoppingmall/exception/type/ErrorCode.java
@@ -6,7 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-	MEMBER_NOT_FOUND("사용자가 없습니다.");
+	MEMBER_NOT_FOUND("사용자가 없습니다."),
+	ALREADY_MEMBER_EMAIL_EXIST("이미 가입된 이메일이 존재합니다."),
+	ALREADY_MEMBER_USERNAME_EXIST("이미 가입된 아이디가 존재합니다."),
+	PASSWORD_NOT_MATCHED("비밀번호가 일치하지 않습니다.")
+	;
 
 	private final String description;
 }
--- src/main/java/com/gang/oneclickshoppingmall/member/service/MemberService.java
+++ src/main/java/com/gang/oneclickshoppingmall/member/service/MemberService.java
@@ -1,8 +1,8 @@
 package com.gang.oneclickshoppingmall.member.service;
 
-import static com.gang.oneclickshoppingmall.exception.type.ErrorCode.MEMBER_NOT_FOUND;
+import static com.gang.oneclickshoppingmall.exception.type.ErrorCode.*;
 
-import com.gang.oneclickshoppingmall.exception.type.CustomException;
+import com.gang.oneclickshoppingmall.exception.CustomException;
 import com.gang.oneclickshoppingmall.jwt.JwtTokenProvider;
 import com.gang.oneclickshoppingmall.member.dto.LoginDto.LoginRequestDto;
 import com.gang.oneclickshoppingmall.member.dto.SignUpDto.SignUpRequestDto;
@@ -23,6 +23,8 @@ public class MemberService {
 	private final JwtTokenProvider jwtTokenProvider;
 
 	public Long registerMember(SignUpRequestDto requestDto) {
+		validateRegisterMember(requestDto);
+
 		Member newMember = Member.builder()
 			.username(requestDto.getUsername())
 			.email(requestDto.getEmail())
@@ -42,5 +44,17 @@ public class MemberService {
 			.orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
 
 		return jwtTokenProvider.createToken(loginMember.getEmail(), loginMember.getRoles());
+	}
+
+	private void validateRegisterMember(SignUpRequestDto request) {
+		if (memberRepository.findByEmail(request.getEmail()).isPresent()) {
+			throw new CustomException(ALREADY_MEMBER_EMAIL_EXIST);
+		}
+		if (!request.getPassword().equals(request.getCheckedPassword())) {
+			throw new CustomException(PASSWORD_NOT_MATCHED);
+		}
+		if (memberRepository.findByUsername(request.getUsername()).isPresent()) {
+			throw new CustomException(ALREADY_MEMBER_USERNAME_EXIST);
+		}
 	}
 }
